c26ff2ffae1d5963707ffe82e25b17d68284a60c
Sebi Nemeth <>
2021-05-04 13:56:03 +0200

    global access

--- client/network.py
+++ client/network.py
@@ -4,7 +4,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "localhost"  # "192.168.0.141"
+        self.server = "chesscraft.remetelak.com"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
