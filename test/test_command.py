b99871f831393d9ae1401f8b3a562f18e29f04a5
Sebi Nemeth <>
2021-05-04 12:21:52 +0200

    more tests

--- test/test_command.py
+++ test/test_command.py
@@ -2,19 +2,49 @@ from command import *
 from game import Game
 
 
-def test_command_parse():
+def test_command_parse_none():
     assert Command.parse("NONE:").type == CommandType.NONE
+
+
+def test_command_parse_hello():
     assert Command.parse("HELLO:").type == CommandType.HELLO
+
+
+def test_command_parse_state():
     assert Command.parse("STATE:").type == CommandType.STATE
+
+
+def test_command_parse_wait():
     assert Command.parse("WAIT:").type == CommandType.WAIT
+
+
+def test_command_parse_step():
     assert Command.parse("STEP:").type == CommandType.STEP
+
+
+def test_command_parse_ping():
     assert Command.parse("PING:").type == CommandType.PING
 
 
-def test_command_construct():
+def test_command_construct_none():
     assert Command().type == CommandType.NONE
+
+
+def test_command_construct_hello():
     assert HelloCommand().type == CommandType.HELLO
+
+
+def test_command_construct_state():
     assert GameStateCommand(Game()).type == CommandType.STATE
+
+
+def test_command_construct_wait():
     assert WaitCommand().type == CommandType.WAIT
+
+
+def test_command_construct_step():
     assert StepCommand().type == CommandType.STEP
+
+
+def test_command_construct_ping():
     assert PingCommand().type == CommandType.PING
